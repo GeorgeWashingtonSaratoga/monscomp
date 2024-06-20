76377a7c2dd482b2001f4033fb7f1751015ff405
George Washington <112564097+GeorgeWashingtonSaratoga@users.noreply.github.com>
2024-06-19 22:43:20 -0400

    Add files via upload

---
+++ ethMonsters.js
@@ -0,0 +1,219 @@
+const monsters = [
+    {
+        name: "Ghazt",
+        image: "",
+        elements: "Plasma",
+        island: "Plant, Ethereal, Colossingum, Composer",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Grumpyre",
+        image: "",
+        elements: "Shadow",
+        island: "Cold, Ethereal, Colossingum",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Reebro",
+        image: "",
+        elements: "Mech",
+        island: "Air, Ethereal, Colossingum",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Jeeode",
+        image: "",
+        elements: "Crystal",
+        island: "Water, Ethereal, Colossingum",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Humbug",
+        image: "",
+        elements: "Poison",
+        island: "Earth, Ethereal, Colossingum",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Whisp",
+        image: "",
+        elements: "Plasma, Shadow",
+        island: "Ethereal, Colossingum",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Neoblob",
+        image: "",
+        elements: "Plasma, Mech",
+        island: "Ethereal, Colossingum",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Sox",
+        image: "",
+        elements: "Plasma, Crystal",
+        island: "Ethereal, Colossingum",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Jellbilly",
+        image: "",
+        elements: "Plasma, Poison",
+        island: "Ethereal, Colossingum",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Arackulele",
+        image: "",
+        elements: "Shadow, Mech",
+        island: "Ethereal, Colossingum",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Boodoo",
+        image: "",
+        elements: "Shadow, Crystal",
+        island: "Ethereal, Colossingum",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Kazilleon",
+        image: "",
+        elements: "Shadow, Poison",
+        island: "Ethereal, Colossingum",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Bellowfish",
+        image: "",
+        elements: "Mech, Crystal",
+        island: "Ethereal, Colossingum",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Dragong",
+        image: "",
+        elements: "Mech, Poison",
+        island: "Ethereal, Colossingum",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Fungpray",
+        image: "",
+        elements: "Crystal, Poison",
+        island: "Ethereal, Colossingum",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Yooreek",
+        image: "",
+        elements: "Plasma, Shadow, Mech",
+        island: "Workshop",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Meebkin",
+        image: "",
+        elements: "Plasma, Shadow, Crystal",
+        island: "Workshop",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Blarret",
+        image: "",
+        elements: "Plasma, Shadow, Poison",
+        island: "Workshop",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Gaddzooks",
+        image: "",
+        elements: "Plasma, Mech, Crystal",
+        island: "Workshop",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Auglur",
+        image: "",
+        elements: "Plasma, Mech, Poison",
+        island: "Workshop",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Flasque",
+        image: "",
+        elements: "Plasma, Crystal, Poison",
+        island: "Workshop",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Nitebear",
+        image: "",
+        elements: "Shadow, Mech, Crystal",
+        island: "Workshop",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Piplash",
+        image: "",
+        elements: "Shadow, Mech, Poison",
+        island: "Workshop",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "X'rt",
+        image: "",
+        elements: "Shadow, Crystal, Poison",
+        island: "Workshop",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Teeter-Tauter",
+        image: "",
+        elements: "Mech, Crystal, Poison",
+        island: "Workshop",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Whaill",
+        image: "",
+        elements: "Plasma, Shadow, Mech, Crystal",
+        island: "Workshop",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Vhenshun",
+        image: "",
+        elements: "Plasma, Shadow, Mech, Poison",
+        island: "Workshop",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Pentumbra",
+        image: "",
+        elements: "Plasma, Shadow, Crystal, Poison",
+        island: "Workshop",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Rhysmuth",
+        image: "",
+        elements: "Plasma, Mech, Crystal, Poison",
+        island: "Workshop",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "???",
+        image: "",
+        elements: "Shadow, Mech, Crystal, Poison",
+        island: "Workshop",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Whaill",
+        image: "",
+        elements: "Plasma, Shadow, Mech, Crystal, Poison",
+        island: "Workshop",
+        likes: "PLACEHOLDER"
+    }
+]
---
+++ ethereal.js
@@ -0,0 +1,86 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const elementFilters = document.querySelectorAll('.element-filter');
+    const numElementsFilter = document.getElementById('num-elements-filter');
+    const numElementsValue = document.getElementById('num-elements-value');
+    const monsterCardsContainer = document.getElementById('monster-cards-container');
+    const noResultsMessage = document.getElementById('no-results-message');
+    const islandFilter = document.getElementById('islandFilter');
+
+    function applyFilters() {
+        const selectedElements = Array.from(elementFilters)
+            .filter(element => element.checked)
+            .map(element => element.value);
+        const selectedNumElements = numElementsValue.value;
+        const selectedNumFilter = numElementsFilter.value;
+
+        let filteredMonsters = [];
+
+        filteredMonsters = monsters.filter(monster => {
+            const cardElements = monster.elements.split(', ');
+            const cardIslands = monster.island.split(', ');
+            const cardNumElements = cardElements.length;
+            console.log(cardIslands);
+            console.log(islandFilter.value);
+
+            const elementMatch = selectedElements.every(element => cardElements.includes(element));
+            const isIslandMatch = islandFilter.value == "Any" || cardIslands.includes(islandFilter.value);
+            let numElementsMatch;
+            switch(selectedNumFilter) {
+                case '=':
+                    numElementsMatch = cardNumElements === parseInt(selectedNumElements);
+                    break;
+                case '<':
+                    numElementsMatch = cardNumElements < parseInt(selectedNumElements);
+                    break;
+                case '>':
+                    numElementsMatch = cardNumElements > parseInt(selectedNumElements);
+                    break;
+                default:
+                    numElementsMatch = true;
+                    break;
+            }
+
+            return elementMatch && numElementsMatch&& isIslandMatch;
+        });
+
+        // Display filtered monsters or no results message
+        if (filteredMonsters.length > 0) {
+            displayMonsterCards(filteredMonsters);
+            noResultsMessage.style.display = 'none';
+        } else {
+            monsterCardsContainer.innerHTML = '';
+            noResultsMessage.style.display = 'block';
+        }
+    }
+
+    function displayMonsterCards(monsters) {
+        monsterCardsContainer.innerHTML = '';
+        monsters.forEach(monster => {
+            monsterClass = "Ethereal";
+            const newMonsterCard = document.createElement('div');
+            newMonsterCard.classList.add('monster-card');
+            newMonsterCard.setAttribute('data-elements', monster.elements);
+            newMonsterCard.setAttribute('data-num-elements', monster.elements.split(',').length); // Count number of elements
+            newMonsterCard.innerHTML = `
+                <h2 class="monster-name">${monster.name}</h2>
+                <div class="monster-info">
+                    <p><strong>Species:</strong> ${monster.name}</p>
+                    <p><strong>Class:</strong> ${monsterClass}</p>
+                    <p><strong>Elements:</strong> ${monster.elements}</p>
+                    <p><strong>Islands:</strong> ${monster.island}</p>
+                    <p><strong>Likes:</strong> ${monster.likes}</p>
+                </div>
+            `; 
+            // <img src=${monster.image}, alt=${monster.name}>
+            monsterCardsContainer.appendChild(newMonsterCard);
+        });
+    }
+
+    elementFilters.forEach(filter => filter.addEventListener('change', applyFilters));
+    numElementsFilter.addEventListener('change', applyFilters);
+    numElementsValue.addEventListener('change', applyFilters);
+    islandFilter.addEventListener('change', applyFilters);
+
+    // Initial filter application
+    applyFilters();
+});
