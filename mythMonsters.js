1cb086c5e4264e543af13902cf0878a99ad6860d
George Washington <112564097+GeorgeWashingtonSaratoga@users.noreply.github.com>
2024-06-21 16:59:19 -0400

    Add files via upload

---
+++ mythMonsters.js
@@ -0,0 +1,107 @@
+const monsters = [
+    {
+        name: "G'joob",
+        image: "",
+        elements: "Mythical",
+        island: "Plant, Mythical",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Yawstrich",
+        image: "",
+        elements: "Mythical",
+        island: "Air, Mythical",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Strombonin",
+        image: "",
+        elements: "Mythical",
+        island: "Cold, Mythical",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Anglow",
+        image: "",
+        elements: "Mythical",
+        island: "Water, Mythical",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Hyehehe",
+        image: "",
+        elements: "Mythical",
+        island: "Earth, Mythical",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Buzzinga",
+        image: "",
+        elements: "Mythical",
+        island: "Haven, Mythical",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Cherubble",
+        image: "",
+        elements: "Mythical",
+        island: "Oasis, Mythical",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Cataliszt",
+        image: "",
+        elements: "Dreamythical",
+        island: "Mythical",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Bleatnik",
+        image: "",
+        elements: "Dreamythical, Mythical",
+        island: "Mythical",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Sporerow",
+        image: "",
+        elements: "Dreamythical, Mythical",
+        island: "Mythical",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Cranchee",
+        image: "",
+        elements: "Dreamythical, Mythical",
+        island: "Mythical",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Pinghound",
+        image: "",
+        elements: "Dreamythical, Mythical",
+        island: "Mythical",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Wheezel",
+        image: "",
+        elements: "Dreamythical, Mythical",
+        island: "Mythical",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Knurv",
+        image: "",
+        elements: "Dreamythical, Mythical",
+        island: "Mythical",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "shLep",
+        image: "",
+        elements: "Dreamythical, Mythical",
+        island: "Mythical",
+        likes: "PLACEHOLDER"
+    }
+]
