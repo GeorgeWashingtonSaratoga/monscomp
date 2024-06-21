fdb152fdbbaa83bd64169d275bf8bf2fc47c5e1f
George Washington <112564097+GeorgeWashingtonSaratoga@users.noreply.github.com>
2024-06-21 17:49:21 -0400

    Add files via upload

---
+++ seaMonsters.js
@@ -0,0 +1,107 @@
+const monsters = [
+    {
+        name: "Punkleton",
+        image: "",
+        elements: "Spooktacle",
+        island: "Plant, Shanty, Colossingum, Gold",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Yool",
+        image: "",
+        elements: "Festival of Yay",
+        island: "Cold, Shanty, Colossingum, Gold",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Smoochle",
+        image: "",
+        elements: "Season of Love",
+        island: "Air, Shanty, Colossingum, Gold",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Blabbit",
+        image: "",
+        elements: "Eggs-Travaganza",
+        island: "Water, Shanty, Colossingum, Gold",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Hoola",
+        image: "",
+        elements: "SummerSong",
+        island: "Air, Earth, Shanty, Colossingum, Gold",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "GobbleyGourd",
+        image: "",
+        elements: "Feast-Ember",
+        island: "Haven, Oasis, Shanty",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Jam Boree",
+        image: "",
+        elements: "Anniversary Month",
+        island: "Shanty, Gold",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Clavavera",
+        image: "",
+        elements: "Beat Hereafter",
+        island: "Bone, Shanty",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Carillong",
+        image: "",
+        elements: "Crescendo Moon",
+        island: "Sanctum, Shanty",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Ffidyll",
+        image: "",
+        elements: "Cloverspell",
+        island: "Faerie, Shanty",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Viveine",
+        image: "",
+        elements: "Echoes of Eco",
+        island: "Shugabush, Shanty, Amber",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Spurrit",
+        image: "",
+        elements: "Perplexplore",
+        island: "Oasis, Shanty",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Monculus",
+        image: "",
+        elements: "Life-Formula",
+        island: "Ethereal, Wublin, Shanty",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Whiz-bang",
+        image: "",
+        elements: "SkyPainting",
+        island: "Light, Shanty",
+        likes: "PLACEHOLDER"
+    },
+    {
+        name: "Boo'qwurm",
+        image: "",
+        elements: "MindBoggle",
+        island: "Psychic, Shanty",
+        likes: "PLACEHOLDER"
+    }
+]
